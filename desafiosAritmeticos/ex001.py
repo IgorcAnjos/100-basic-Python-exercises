40338da539c2cf0d058653befa251cb37af6aace
Igor Leandro <igorcatuliodosanjos@gmail.com>
2022-04-07 20:53:26 -0300

    first commit

---
+++ desafiosAritmeticos/ex001.py
@@ -0,0 +1,4 @@
+# Questão 1) Faça um programa que leia um numero inteiro e mostre na tela seu sucessor, e seu antecessor.
+
+n1=int(input('Digite um número inteiro: '))
+print('seu sucessor é {}, e seu antecessor é {}'.format(n1+1, n1-1))
---
+++ desafiosAritmeticos/ex002.py
@@ -0,0 +1,3 @@
+# Questão 2) Crie um algoritmo que leia um número e mostre o dobro seu triplo e a raiz quadrada.
+n1=float(input('Digite um número qualquer :'))
+print('Seu dobro é {}, seu triplo é {}, e sua raiz quadrada é {}'.format(n1*2, n1*3, n1**(1/2)))
